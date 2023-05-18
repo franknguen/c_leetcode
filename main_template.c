f42de48f611d33a3ff142e5f172e2020fb982509
Frank NGUEN <nguyenmanhduc18@gmail.com>
2023-05-19 01:16:47 +0900

    frk

--- main_template.c
+++ main_template.c
@@ -1,26 +1,52 @@
 /***********************************************************************/
 /**
- * main - main program, test case
- * problems: convert a string into zigzag string
- */
+    LeetCode 
+    Purpose: #94, binary tree inorder traversal
+    @author: Frank Nguen
+    @date:   0x-05-2023
+    https://www.geeksforgeeks.org/inorder-tree-traversal-without-recursion/
+*/
 /***********************************************************************/
 #if 1
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
+/***********************************************************************/
+// funs declerations
+/***********************************************************************/
+
 void printChar(char* s);
 
-int main(){
+/***********************************************************************/
+/**
+ * main - main program, test case
+ * problems: convert a string into zigzag string
+ */
+/***********************************************************************/
+
+int main()
+{
+    printf("\n start testing ....................................");
+    printf("\n---------------------------------------------------");
+    
     char a[] = "0123456789";
+    
     printf("---------------------------------------------------\n");
     printChar(a);    
+
     printf("\n---------------------------------------------------");
     printf("\nPASS-TEST\n");
     return 0;
 }
+
 #endif
+
 /***********************************************************************/
+// funs implements
+/***********************************************************************/
+
 /**
  * printChar - print a string
  * @s:       input string
@@ -28,7 +54,9 @@ int main(){
  */
 void printChar(char* s)
 {
+
     int lengs = strlen(s);
+    
     for(int i=0; i<lengs; i++)
     {
         printf("%c", s[i]);
